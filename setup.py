e83b55c124e535eab9a4768f1612f77a5740feee
Harsh <bhatiharsh102@gmail.com>
2025-10-31 12:04:19 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="interactive_chatbot",
+    version="0.1.0",
+    author="Harsh Bhati",
+    packages=find_packages(),
+    install_requires=[]
+)
+    
